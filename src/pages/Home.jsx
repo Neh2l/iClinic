d700096b76123b498f592da227b893625508926c
Nehal Reda <nehalreda14@gmail.com>
2025-09-19 21:52:16 +0300

    fix-responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,78 +1,84 @@
-import React from 'react'
-import Card from '../components/ui/Card'
-import { BiSolidSkipNextCircle } from 'react-icons/bi';
-import { BiSolidSkipPreviousCircle } from 'react-icons/bi';
+import React from 'react';
+import Card from '../components/ui/Card';
+import { BiSolidSkipNextCircle, BiSolidSkipPreviousCircle } from 'react-icons/bi';
 
 const Carousel = () => {
   return (
-    <div className='d-block  justify-content-center align-items-center' >
-    
-    <div 
-      id="carouselExample" 
-      className="carousel slide d-flex justify-content-center align-items-center"
-      data-bs-ride="carousel"
-      style={{ height: "" }}
-    >
-      
-      <div className="carousel-inner" style={{ width: "80%" }}>
-        
-        
-        <div className="carousel-item active">
-          <div className="d-flex justify-content-center gap-3">
-            <Card />
-            <Card />
-             <Card />
-            <Card />
+    <div className="container text-center my-5">
+      <h3 className="mb-4 w-75 mx-auto">
+        check some of our <span className="primary-text">reviews</span> down here...
+      </h3>
+
+      <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+
+          <div className="carousel-item active">
+            <div className="d-flex justify-content-center flex-nowrap gap-3">
+
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+
+
+              <div className="d-none d-md-flex d-lg-none col-md-6"><Card /></div>
+              <div className="d-none d-md-flex d-lg-none col-md-6"><Card /></div>
+
+
+              <div className="d-flex d-md-none justify-content-center col-10 mx-2"><Card /></div>
+            </div>
           </div>
+
+
+          <div className="carousel-item">
+            <div className="d-flex justify-content-center flex-nowrap gap-3">
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+
+              <div className="d-none d-md-flex d-lg-none col-md-6"><Card /></div>
+              <div className="d-none d-md-flex d-lg-none col-md-6"><Card /></div>
+
+              <div className="d-flex d-md-none justify-content-center col-10 mx-2"><Card /></div>
+            </div>
+          </div>
+
+          <div className="carousel-item">
+            <div className="d-flex justify-content-center flex-nowrap gap-3">
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+              <div className="d-none d-lg-block col-lg-3"><Card /></div>
+
+              <div className="d-none d-md-flex d-lg-none col-md-6"><Card /></div>
+              <div className="d-none d-md-flex d-lg-none col-md-6"><Card /></div>
+
+              <div className="d-flex d-md-none justify-content-center col-10 mx-2"><Card /></div>
+            </div>
+          </div>
+
         </div>
 
-      
-        <div className="carousel-item">
-          <div className="d-flex justify-content-center gap-3">
-            <Card />
-            <Card />
-            <Card />
-             <Card />
-          </div>
-        </div>
-
-        
-        <div className="carousel-item">
-          <div className="d-flex justify-content-center gap-3">
-            <Card />
-            <Card />
-            <Card />
-             <Card />
-          </div>
-        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span><BiSolidSkipPreviousCircle style={{ color: "black", fontSize: "3rem" }} /></span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span><BiSolidSkipNextCircle style={{ color: "black", fontSize: "3rem" }} /></span>
+        </button>
 
       </div>
-
-     
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="">
-          <BiSolidSkipPreviousCircle style={{color:"black",fontSize:"3rem"}}/>
-        </span>
-      </button>
-
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="">
-          <BiSolidSkipNextCircle style={{color:"black",fontSize:"3rem"}}/>
-        </span>
-      </button>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-const Home = () => {
-  return (
-    <>
-      <Carousel />
-    </>
-  )
-}
+const Home = () =>{
+  return(
 
-export default Home
+    <Carousel />
+  );
+} 
+
+export default Home;
