fe935446a2963c27b42be929e918f450d6c5998a
Nehal Reda <nehalreda14@gmail.com>
2025-09-22 03:08:55 +0300

    edit responsive

--- src/pages/Forgetpassword.jsx
+++ src/pages/Forgetpassword.jsx
@@ -4,81 +4,78 @@ import { useNavigate } from 'react-router-dom';
 import Img from '../images/forgetpass.jpg'
 import { toast } from 'react-toastify';
 
-
-
 const CreateNewPassword = () => {
-  const [password,setpassword]=useState('');
-  const [confirm,setconfirm]=useState('');
-  const [error,seterror]=useState('');
-  const navigate=useNavigate();
-  const handlesubmit=(e)=>{
+  const [password, setpassword] = useState('');
+  const [confirm, setconfirm] = useState('');
+  const [error, seterror] = useState('');
+  const navigate = useNavigate();
+
+  const handlesubmit = (e) => {
     e.preventDefault();
-    if(!password||!confirm){
+    if (!password || !confirm) {
       seterror('Both fields are required!')
       return
     }
-     if(password!==confirm){
-      seterror('passwords do not match!')
+    if (password !== confirm) {
+      seterror('Passwords do not match!')
       return
     }
-     if(password.length<6){
-      seterror('password is weak!')
+    if (password.length < 6) {
+      seterror('Password is weak!')
       return
     }
     seterror('')
-    toast.success('password created sucessfully!');
+    toast.success('Password created successfully!');
     navigate('/login');
-    
   }
 
   return (
-    <div className='container d-flex align-items-center g-3 text-center'>
-            <div>
+    <div className="container text-center p-5">
+      <div className="row align-items-center">
+        
+        <div className="col-12 col-md-6  ">
+          <h3>Create new password</h3>
+          <p>Please create new password for your account, make sure it's strong and secure</p>
 
-                 <div>
-                    <h3>Create new password</h3>
-                     <p>please create new password for your account , make sure it's strong and secure</p>
-                 </div>
-                 <form className="d-flex flex-column gap-3" onSubmit={handlesubmit}>
-     
-                 <>
-                 <div className='text-start d-flex flex-column gap-2'>
-                 <label htmlFor="createpass">New password</label>
-                    <input
-                    id='createpass'
-                     type="password"
-                      placeholder="Create new Password"
-                     className="form-control"
-                     value={password}
-                     onChange={(e)=>setpassword(e.target.value)} />
-                  </div>
-                <div className='text-start d-flex flex-column gap-2'>
-                      <label  htmlFor="confirm ">Confirm New password</label>
-                    <input
-                    id='confirm'
-                    type="password"
-                    placeholder="Confirm new Password"
-                    className="form-control"
-                    value={confirm}
-                    onChange={(e)=>setconfirm(e.target.value)}
-                    />
-                    {error && <p style={{ color: "red" }}>{error}</p>}
-
-                </div>
-                    <button type='submit' className="primary-btn  w-100 mt-3 p-2  ">Reset Password</button>
-
-                    <p className='secondary-text ' onClick={()=>{navigate('/login')}}>Back to login ?</p>
-                 </>
-        </form>
-     
+          <form className="d-flex flex-column gap-4" onSubmit={handlesubmit}>
+            <div className='text-start d-flex flex-column gap-2'>
+              <label htmlFor="createpass">New password</label>
+              <input
+                id='createpass'
+                type="password"
+                placeholder="Create new Password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setpassword(e.target.value)} />
             </div>
-            <div>
-                <img src={Img} alt="forgetpass" />
+
+            <div className='text-start d-flex flex-column gap-2'>
+              <label htmlFor="confirm">Confirm New password</label>
+              <input
+                id='confirm'
+                type="password"
+                placeholder="Confirm new Password"
+                className="form-control"
+                value={confirm}
+                onChange={(e) => setconfirm(e.target.value)}
+              />
             </div>
-      
+
+            {error && <p style={{ color: "red" }}>{error}</p>}
+
+            <button type='submit' className="primary-btn w-100 mt-3 p-2">Reset Password</button>
+            <p className='secondary-text mt-2' onClick={() => navigate('/login')}>Back to login ?</p>
+          </form>
+        </div>
+
+        <div className="col-12 col-md-6 d-none d-md-block">
+          <img src={Img} alt="forgetpass" className="img-fluid" />
+        </div>
+      </div>
     </div>
   )
 }
+
 
 const Verification = () => {
   return (
