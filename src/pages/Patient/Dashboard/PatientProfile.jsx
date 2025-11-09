177750e942779be311347bc5b69c882eb6d9826b
youstina533 <youstinaabdelmeseh@yahoo.com>
2025-11-10 01:31:55 +0200

    Edit Patient Profile

--- src/pages/Patient/Dashboard/PatientProfile.jsx
+++ src/pages/Patient/Dashboard/PatientProfile.jsx
@@ -20,7 +20,7 @@ export default function PatientProfile() {
                     <div class="col-12 col-lg-6">
                         <div className="patientDetails pt-1">
                             <div id="patientName">
-                                <h1>John Andreson</h1>
+                                <h1>Tia Andreson</h1>
                             </div>
                             <div className="patientInfo mt-3">
                                 <div className="div-info mb-3">
@@ -33,7 +33,7 @@ export default function PatientProfile() {
                                     <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" className="bi bi-envelope svg-info" viewBox="0 0 16 16">
                                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
                                     </svg>
-                                    <p className="email ms-3 mt-5">Johna@gmail.com</p>
+                                    <p className="email ms-3 mt-5">Tiaaaa@gmail.com</p>
                                 </div>
                                 <div className="div-info mb-3">
                                     <svg  xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" className="bi bi-telephone svg-info" viewBox="0 0 16 16">
@@ -48,39 +48,69 @@ export default function PatientProfile() {
 
                 </div>  
 
-                <div className="another-info row mt-4 d-flex justify-content-center">
+                <div className="another-info row mt-4 d-flex justify-content-evenly g-2">
 
-                    <div className="birthDate info col-lg-3 col-12 rounded-4 me-5 p-4">
+                    <div className="birthDate info col-lg-3 col-12 rounded-4 p-4">
                         <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" fill="white" className="bi bi-calendar-check svg-another-info" viewBox="0 0 16 16">
                         <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
                         <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
                         </svg>
-                        <p>Date of Birth</p>
-                        <h3 className="mt-3 ms-4">March 15, 1985</h3>
-
+                        <p className="title">Date of Birth</p>
+                        <p className="mt-3 ms-4 answer">March 15, 1985</p>
                     </div>
-                    <div className="bloodType info col-lg-3 col-12 rounded-4 me-5 p-4">
+                    <div className="bloodType info col-lg-3 col-12 rounded-4 p-4">
                         <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" fill="white" className="bi bi-droplet svg-another-info" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0q.164.544.371 1.038c.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8m.413 1.021A31 31 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
                         <path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87z"/>
                         </svg>
-                        <p>Blood Type</p>
-                        <h3 className="mt-3 ms-4">A+</h3>
-
+                        <p className="title">Blood Type</p>
+                        <p className="mt-3 ms-4 answer ">A+</p>
                     </div>
                     <div className="allegies info col-lg-3 col-12 rounded-4 p-4">
                         <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" fill="white" className="bi bi-exclamation-circle svg-another-info" viewBox="0 0 16 16">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                         <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
                         </svg>
-                        <p>Allegies</p>
-                        <h3 className="mt-3 ms-4">Penicillin</h3>
+                        <p className="title">Allegies</p>
+                        <p className="mt-3 ms-4 answer">Penicillin</p>
+                    </div>
 
+                </div>
+            </div>
+             <div className="scores row mt-4 mb-5 d-flex justify-content-evenly g-2">
+                    <div className="health score  col-lg-3 col-12 rounded-4 p-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-heart" viewBox="0 0 16 16">
+                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="rgb(243, 226, 47)" className="bi bi-star-fill float-end" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        <p className="title2">Health Score</p>
+                        <p className="mt-3 ms-4 answer2">92%</p>
+                    </div>
+                    <div className="Active-visit score col-lg-3 col-12 rounded-4 p-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#00A6CE" class="bi bi-activity" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="rgb(243, 226, 47)" className="bi bi-star-fill float-end" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        <p className="title2">Active Visits</p>
+                        <p className="mt-3 ms-4 answer2">12</p>
+                    </div>
+                    <div className=" streak-days score col-lg-3 col-12 rounded-4 p-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="rgb(243, 226, 47)" className="bi bi-star-fill float-end" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        <p className="title2">Streak Days</p>
+                        <p className="mt-3 ms-4 answer2">45</p>
                     </div>
 
                 </div>
 
-            </div>
         </div>
         </PatientLayout>
     </div>
